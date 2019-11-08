f07d3bb98d9f5369aa53a535b9ef3e797f2919d0
Uma <buldog52@gmail.com>
2019-11-08 13:55:37 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+window.$ = window.jQuery = require('jquery');
+
+import './components/main-header/main-header';
+import './components/menu-toggle/menu-toggle';
+import './components/modal/modal';
+import './components/callback/callback';
