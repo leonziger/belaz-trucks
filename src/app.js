f29f672a786d8dbc86196eb822fc713a7baae8bf
Uma <buldog52@gmail.com>
2019-11-11 17:38:30 +0200

    .wide-range finished

--- src/app.js
+++ src/app.js
@@ -4,3 +4,4 @@ import './components/main-header/main-header';
 import './components/menu-toggle/menu-toggle';
 import './components/modal/modal';
 import './components/callback/callback';
+import './components/scroll-to-anchor/scroll-to-anchor';
---
+++ src/components/scroll-to-anchor/scroll-to-anchor.js
@@ -0,0 +1,18 @@
+import $ from 'jquery';
+
+const screenWidth = $(window).width();
+if ( screenWidth <= 991) {
+    $('[href^="#"]').click(function(){
+        const mainHeader = document.querySelector('.main-header');
+        const mainHeaderPosition = getComputedStyle(mainHeader).position;
+        const link = $(this).attr('href');
+        let coordinates = $(link).offset().top;
+        // console.log(coordinates);
+
+        if ( mainHeaderPosition === 'fixed') {
+            coordinates = $(link).offset().top-$('.main-header').height();
+        }
+
+        $('html, body').animate({scrollTop:coordinates}, 1000);
+    });
+}
