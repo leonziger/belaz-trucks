334cba49cd713e914352a25c191261914963fbeb
Uma <buldog52@gmail.com>
2019-11-12 14:19:37 +0200

    Added .callback modal window

--- src/app.js
+++ src/app.js
@@ -9,3 +9,8 @@ import './components/scroll-to-anchor/scroll-to-anchor';
 import './components/scroll-up/scroll-up';
 import './components/get-price/get-price';
 import './components/parts-search/parts-search';
+
+
+$('.safe-transportation__advantage-link').click(function() {
+    return false;
+});
--- src/components/main-header/main-header.js
+++ src/components/main-header/main-header.js
@@ -3,7 +3,7 @@ import vex from 'vex-js';
 import callback from '../callback/callback';
 
 
-$('.main-header__callback-link').click(function (e) {
+$('.main-header__contacts-button').click(function (e) {
     e.preventDefault();
     const modal = $(`[data-modal=callback]`);
 
@@ -15,7 +15,54 @@ $('.main-header__callback-link').click(function (e) {
         unsafeContent: modal.html(),
         closeClassName: 'modal__close',
         afterOpen: function () {
-            callback.mask();
+            const cbForm = $('.callback__form');
+            const phone = $('[name="phone"]');
+            const fieldErrorClassName = 'callback__phone-error';
+            const fieldValidClassName = 'callback__phone-valid';
+            phone.mask('+380 (99) 999-99-99', { autoclear: false });
+
+            $.validator.addMethod('condition', function(value, element, condition) {
+                if (typeof condition !== 'function') {
+                    throw new Error('"condition" rule must return a function');
+                }
+                return this.optional(element) || condition(value);
+            });
+
+            cbForm.validate({
+                rules: {
+                    'phone': {
+                        required: true,
+                        condition: () => (value) => value.indexOf('_') === -1
+                    }
+                },
+
+                messages: {
+                    'phone': {
+                        required: 'Обязательное поле для заполнения',
+                        condition: 'Пожалуйста, введите 9 цифр номера Вашого телефона'
+                    }
+                },
+
+                highlight: (element) => {
+                    $(element).addClass(fieldErrorClassName).removeClass(fieldValidClassName);
+                },
+
+                unhighlight: (element) => {
+                    $(element).removeClass(fieldErrorClassName).addClass(fieldValidClassName);
+                },
+
+                errorPlacement: function(error, element) {
+                    error.addClass('callback__error-message');
+                    error.insertAfter(element);
+                },
+
+                submitHandler: function(form) {
+                    // openThanksModal();
+                    cbForm.trigger('reset');
+                    $('.modal__close').trigger('click');
+
+                }
+            });
         }
     });
 });
